0367ad7a80fe9d0db9e69cad981650b0af4f68d8
Viruss-git <sergei-6@bk.ru>
2022-06-22 10:09:55 +0300

    feat: task done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 module.exports = function reverse(n) {
     if (n < 0) {
-        return Math.abs(n);
+        let a = [...String(Math.abs(n))].reverse().join('');
+        let result = Number(a);
+        return result;
     } else {
         let a = [...String(n)].reverse().join('');
         let result = Number(a);

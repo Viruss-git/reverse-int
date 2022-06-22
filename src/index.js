51c9da0dfa0f7e55c62e240b0d0e9e7809762013
Viruss-git <sergei-6@bk.ru>
2022-06-22 10:24:09 +0300

    feat: optimisation code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 module.exports = function reverse(n) {
-    if (n < 0) {
         let a = [...String(Math.abs(n))].reverse().join('');
         let result = Number(a);
         return result;
-    } else {
-        let a = [...String(n)].reverse().join('');
-        let result = Number(a);
-        return result;
-    }
 }
 

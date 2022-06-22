0c436aaf7c55f020371d7f3c9c39c8b5c47aa61c
Viruss-git <sergei-6@bk.ru>
2022-06-22 09:07:05 +0300

    feat: add result

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    if (n < 0) {
+        return Math.abs(n);
+    } else {
+        let a = [...String(n)].reverse().join('');
+        let result = Number(a);
+        return result;
+    }
 }
+
